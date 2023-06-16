4976c636c64bdc4d40f0b53b31af3214a3c89733
Davide Francesco Riccio <ledrazors@gmail.com>
2023-06-16 09:21:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Use a for-loop to print all the number from 5 to 25 (including the upper and lower bound)
+
+        for (int i = 5; i <= 25; i++){
+            System.out.println(i);
+
+
+        }
+    }
+}
